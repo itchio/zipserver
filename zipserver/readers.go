f01e7a7c9441e325aa4b61cf9e307973dc392f38
Amos Wenger <amos@itch.io>
2017-09-12 17:39:42 +0200

    More unit tests & comments, fix limitedReader

--- zipserver/readers.go
+++ zipserver/readers.go
@@ -24,14 +24,11 @@ func annotatedReader(reader io.Reader) readerClosure {
 // wraps a reader to fail if it reads more than max of maxBytes, also tracks
 // the total amount of bytes read
 func limitedReader(reader io.Reader, maxBytes uint64, totalBytes *uint64) readerClosure {
-	remainingBytes := maxBytes
 	return func(p []byte) (int, error) {
 		bytesRead, err := reader.Read(p)
-		remainingBytes -= uint64(bytesRead)
-
 		*totalBytes += uint64(bytesRead)
 
-		if remainingBytes < 0 {
+		if *totalBytes > maxBytes {
 			return bytesRead, fmt.Errorf("File too large (max %d bytes)", maxBytes)
 		}
 
---
+++ zipserver/readers_test.go
@@ -0,0 +1,48 @@
+package zipserver
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_annotatedReader(t *testing.T) {
+	s := "Hello, world"
+
+	sr := bytes.NewReader([]byte(s))
+	ar := annotatedReader(sr)
+
+	buf := make([]byte, 4)
+	var totalBytes int
+	for {
+		n, err := ar.Read(buf)
+		totalBytes += n
+		if err == io.EOF {
+			break
+		}
+		assert.NoError(t, err)
+	}
+	assert.EqualValues(t, len(s), totalBytes)
+}
+
+func Test_limitedReader(t *testing.T) {
+	s := "Hello, world"
+
+	sr := bytes.NewReader([]byte(s))
+	var totalBytes uint64
+	lr := limitedReader(sr, 128, &totalBytes)
+
+	result, err := ioutil.ReadAll(lr)
+	assert.NoError(t, err)
+	assert.EqualValues(t, s, string(result))
+	assert.EqualValues(t, len(s), totalBytes)
+
+	sr.Seek(0, os.SEEK_SET)
+	lr = limitedReader(sr, 5, &totalBytes)
+	result, err = ioutil.ReadAll(lr)
+	assert.Error(t, err)
+}
