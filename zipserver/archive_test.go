afe490e07e1ee75a0a35d02d675fbabd2a16da25
Amos Wenger <amos@itch.io>
2017-09-12 16:43:08 +0200

    Skip test cases when we don't have the hardcoded private key

--- zipserver/archive_test.go
+++ zipserver/archive_test.go
@@ -3,25 +3,26 @@ package zipserver
 import "testing"
 
 func TestExtractZip(t *testing.T) {
-	client, config := getClient(t)
-	archiver := &Archiver{client, config}
+	withClient(t, func(client *StorageClient, config *Config) {
+		archiver := &Archiver{client, config}
 
-	limits := &ExtractLimits{
-		MaxFileSize:       1024 * 1024 * 200,
-		MaxTotalSize:      1024 * 1024 * 500,
-		MaxNumFiles:       100,
-		MaxFileNameLength: 80,
-		ExtractionThreads: 4,
-	}
+		limits := &ExtractLimits{
+			MaxFileSize:       1024 * 1024 * 200,
+			MaxTotalSize:      1024 * 1024 * 500,
+			MaxNumFiles:       100,
+			MaxFileNameLength: 80,
+			ExtractionThreads: 4,
+		}
 
-	err := client.PutFileFromFname(config.Bucket, "zipserver_test/test.zip", "/home/leafo/code/go/etlua.zip")
-	if err != nil {
-		t.Fatal(err)
-	}
+		err := client.PutFileFromFname(config.Bucket, "zipserver_test/test.zip", "/home/leafo/code/go/etlua.zip")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	_, err = archiver.ExtractZip("zipserver_test/test.zip", "zipserver_test/extract", limits)
+		_, err = archiver.ExtractZip("zipserver_test/test.zip", "zipserver_test/extract", limits)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
--- zipserver/storage_client_test.go
+++ zipserver/storage_client_test.go
@@ -1,13 +1,24 @@
 package zipserver
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
 
-func getClient(t *testing.T) (*StorageClient, *Config) {
+const testPrivateKey = "/home/leafo/code/go/cf45ea3f8a5f730a4b9702d11236439d9b014b20-privatekey.pem"
+
+type ClientFunc func(client *StorageClient, config *Config)
+
+func withClient(t *testing.T, cb ClientFunc) {
+	_, err := os.Lstat(testPrivateKey)
+	if err != nil {
+		t.Logf("Skipping %s (no private key)", t.Name())
+		return
+	}
+
 	config := &Config{
-		PrivateKeyPath: "/home/leafo/code/go/cf45ea3f8a5f730a4b9702d11236439d9b014b20-privatekey.pem",
+		PrivateKeyPath: testPrivateKey,
 		ClientEmail:    "507810471102@developer.gserviceaccount.com",
 		Bucket:         "leafo",
 	}
@@ -18,34 +29,35 @@ func getClient(t *testing.T) (*StorageClient, *Config) {
 		t.Fatal(err)
 	}
 
-	return client, config
+	cb(client, config)
 }
 
 func TestGetFile(t *testing.T) {
-	client, config := getClient(t)
+	withClient(t, func(client *StorageClient, config *Config) {
+		str, err := client.GetFileToString(config.Bucket, "text.txt")
 
-	str, err := client.GetFileToString(config.Bucket, "text.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !strings.Contains(str, "Gravity") {
-		t.Fatal("Expected to get string from text.txt")
-	}
+		if !strings.Contains(str, "Gravity") {
+			t.Fatal("Expected to get string from text.txt")
+		}
+	})
 }
 
 func TestPutAndDeleteFile(t *testing.T) {
-	client, config := getClient(t)
-	err := client.PutFile(config.Bucket, "zipserver_test.txt", strings.NewReader("hello zipserver!"), "text/plain")
+	withClient(t, func(client *StorageClient, config *Config) {
+		err := client.PutFile(config.Bucket, "zipserver_test.txt", strings.NewReader("hello zipserver!"), "text/plain")
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	err = client.DeleteFile(config.Bucket, "zipserver_test.txt")
+		err = client.DeleteFile(config.Bucket, "zipserver_test.txt")
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
