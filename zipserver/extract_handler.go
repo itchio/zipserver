76b4c09b5ee2d95528d0fc727f23398162d84c84
leaf corcoran <leafot@gmail.com>
2023-11-21 18:52:42 -0800

    move lock table to a new file

--- zipserver/extract_handler.go
+++ zipserver/extract_handler.go
@@ -8,48 +8,10 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"sync"
 	"time"
 )
 
-type LockTable struct {
-	// maps aren't thread-safe in golang, this protects openKeys
-	sync.Mutex
-	// empty struct is zero-width, we're using that map as a set (no values)
-	openKeys map[string]struct{}
-}
-
-func NewLockTable() *LockTable {
-	return &LockTable{
-		openKeys: make(map[string]struct{}),
-	}
-}
-
-// tryLockKey tries acquiring the lock for a given key
-// it returns true if we successfully acquired the lock,
-// false if the key is locked by someone else
-func (lt *LockTable) tryLockKey(key string) bool {
-	lt.Lock()
-	defer lt.Unlock()
-
-	// test for key existence
-	if _, ok := lt.openKeys[key]; ok {
-		// locked by someone else
-		return false
-	}
-	lt.openKeys[key] = struct{}{}
-	return true
-}
-
-func (lt *LockTable) releaseKey(key string) {
-	lt.Lock()
-	defer lt.Unlock()
-
-	// delete key from map so the map doesn't keep growing
-	delete(lt.openKeys, key)
-}
-
-// create static instance of LockTable for this code file to use
+// mutex for keys currently being extracted
 var extractLockTable = NewLockTable()
 
 func loadLimits(params url.Values, config *Config) *ExtractLimits {
